f449063348761b0c1f56d297a9932b3ed312b74b
limaarthur <artglima.dev@gmail.com>
2025-05-21 20:48:33 -0300

    feat: header component creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import { Header } from './components/Header';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 export function App() {
   return (
     <>
-      <h1>Hello World</h1>
-
+      <Header />
       <GlobalStyles />
     </>
   )
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 5rem;
+  padding: 0 1.8rem;
+  background: #7159c1;
+  color: #FFFFFF;
+  
+  display: flex;
+  align-items: center;
+`;
