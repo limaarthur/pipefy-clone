5cd19144d881a41692845f58008d1f748139361f
limaarthur <artglima.dev@gmail.com>
2025-05-26 15:37:57 -0300

    feat: installing and configuring dnd for drag and drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
 import { Header } from './components/Header';
 import { Board } from './components/Board';
+
 import { GlobalStyles } from './styles/GlobalStyles';
 
 export function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <Board />
       <GlobalStyles />
-    </>
+    </DndProvider>
   )
 }
